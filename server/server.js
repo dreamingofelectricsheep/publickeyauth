464f8a00fed9e4f1bab7bab03d16f34a13c8b847
Michal Kolodziej <softcushiontorture@gmail.com>
2013-05-07 17:30:22 +0200

    Started working on the server.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,36 @@
-var jwcrypto = require('./jwcrypto')
+var jwcrypto = require('jwcrypto')
 
 
+var page_path =
+{
+	'/': 'index.html',
+	'/tags/module.js': 'tags/module.js',
+	'/tags/tags.js': 'tags/tags.js'
+}
+
+var pages = {}
+
+for(var i in page_path)
+{
+	(function(k, v) {
+		fs.readFile(v, function(err, data)
+			{
+				pages[k] = data
+			}
+		})(i, page_path[i])
+}
+
+
+http.createServer(function (req, res)
+	{
+		var f = page[req.url]
+
+		var code = f == undefined ?
+			404 : 200
+
+		res.writeHead(code, {'Content-Type': 'text/html'})
+		res.end(f)
+
+	}).listen(1337)
 
 
